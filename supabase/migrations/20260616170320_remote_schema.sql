a600f709d9d23e0d1b2441abe2f8181274692172
JuanGeniusOps <juan@geniuspro.io>
2026-06-16 12:06:43 -0500

    Add inital migration from existing schema

---
+++ supabase/migrations/20260616170320_remote_schema.sql
@@ -0,0 +1,64 @@
+drop extension if exists "pg_net";
+
+revoke references on table "public"."users" from "anon";
+
+revoke trigger on table "public"."users" from "anon";
+
+revoke truncate on table "public"."users" from "anon";
+
+revoke references on table "public"."users" from "authenticated";
+
+revoke trigger on table "public"."users" from "authenticated";
+
+revoke truncate on table "public"."users" from "authenticated";
+
+revoke references on table "public"."users" from "service_role";
+
+revoke trigger on table "public"."users" from "service_role";
+
+revoke truncate on table "public"."users" from "service_role";
+
+alter table "public"."users" drop constraint "users_email_key";
+
+alter table "public"."users" drop constraint "users_pkey";
+
+drop index if exists "public"."users_email_key";
+
+drop index if exists "public"."users_pkey";
+
+drop table "public"."users";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.rls_auto_enable()
+ RETURNS event_trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO 'pg_catalog'
+AS $function$
+DECLARE
+  cmd record;
+BEGIN
+  FOR cmd IN
+    SELECT *
+    FROM pg_event_trigger_ddl_commands()
+    WHERE command_tag IN ('CREATE TABLE', 'CREATE TABLE AS', 'SELECT INTO')
+      AND object_type IN ('table','partitioned table')
+  LOOP
+     IF cmd.schema_name IS NOT NULL AND cmd.schema_name IN ('public') AND cmd.schema_name NOT IN ('pg_catalog','information_schema') AND cmd.schema_name NOT LIKE 'pg_toast%' AND cmd.schema_name NOT LIKE 'pg_temp%' THEN
+      BEGIN
+        EXECUTE format('alter table if exists %s enable row level security', cmd.object_identity);
+        RAISE LOG 'rls_auto_enable: enabled RLS on %', cmd.object_identity;
+      EXCEPTION
+        WHEN OTHERS THEN
+          RAISE LOG 'rls_auto_enable: failed to enable RLS on %', cmd.object_identity;
+      END;
+     ELSE
+        RAISE LOG 'rls_auto_enable: skip % (either system schema or not in enforced list: %.)', cmd.object_identity, cmd.schema_name;
+     END IF;
+  END LOOP;
+END;
+$function$
+;
+
+
